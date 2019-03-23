d32f38d9b3066a07fea925277895c39de89f5482
Tom Henricksen <tom.henricksen@gmail.com>
2019-03-23 10:47:13 -0400

    Create Customer.java

---
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -0,0 +1,43 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Customer {
+
+	private String firstName;
+	
+	@NotNull(message="is required")
+	@Size(min=1)
+	private String lastName;
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}
