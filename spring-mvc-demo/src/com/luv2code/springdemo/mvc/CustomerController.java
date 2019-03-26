c7fdbba8f315793bee079a80c9c1fab7c7123ee4
Tom Henricksen <tom.henricksen@gmail.com>
2019-03-26 09:34:17 -0400

    Update CustomerController.java

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,7 +1,11 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
