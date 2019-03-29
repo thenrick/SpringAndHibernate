1c2e8fb06d4b1c0888eb2e67dfc099855ce335c0
Tom Henricksen <tom.henricksen@gmail.com>
2019-03-29 09:04:59 -0400

    Update Customer.java

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -1,10 +1,22 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class Customer {
+	@Min(value=0, message="must be greater than or equal to zero")
+	@Max(value=10, message="must be less than or equal to 10")
+	private int freePasses;
 
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
 	private String firstName;
 	
 	@NotNull(message="is required")
