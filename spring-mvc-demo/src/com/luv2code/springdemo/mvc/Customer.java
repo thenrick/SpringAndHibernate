7b18c9b78a2f798e2dc8ac7928418aaa23cfa0f8
Tom Henricksen <tom.henricksen@gmail.com>
2019-03-31 19:02:46 -0400

    Update Customer.java

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -4,12 +4,30 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
 
 public class Customer {
 	@Min(value=0, message="must be greater than or equal to zero")
 	@Max(value=10, message="must be less than or equal to 10")
 	private int freePasses;
+	@Pattern(regexp="^[a-zA-Z0-9] {5}", message="only 5 chars/digits")
+	private String postalCode;
+	
+	/**
+	 * @return the postalCode
+	 */
+	public String getPostalCode() {
+		return postalCode;
+	}
 
+	/**
+	 * @param postalCode the postalCode to set
+	 */
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	
 	public int getFreePasses() {
 		return freePasses;
 	}
