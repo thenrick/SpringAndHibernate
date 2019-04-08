5b50ae9c3e867efeb7e50300e5ff2d844dfdd002
Tom Henricksen <tom.henricksen@gmail.com>
2019-04-08 13:07:49 -0400

    Update and rename spring-mvc-demo/src/com/luv2code/springdemo/mvc/CourseCode.java to spring-mvc-demo/src/com/luv2code/springdemo/mvc/validation/CourseCode.java

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/validation/CourseCode.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/validation/CourseCode.java
@@ -1,4 +1,4 @@
-package com.luv2code.springdemo.mvc;
+package com.luv2code.springdemo.mvc.validation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -6,6 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 @Constraint(validatedBy = CourseCodeConstraintValidator.class)
 @Target({ ElementType.METHOD, ElementType.FIELD })
@@ -15,4 +16,8 @@ public @interface CourseCode {
 	public String value() default "LUV";
 
 	public String message() default "must start with LUV";
+	
+	public Class<?>[] groups() default {};
+	
+	public Class<? extends Payload>[] payload() default {};
 }
